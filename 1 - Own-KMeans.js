b61ac0b30ff81ca15d2fe566e1e304afbca509c0
Filip Vabrousek <filipvabrousek1@gmail.com>
2018-01-11 14:09:55 +0100

    Update 1 - Own-KMeans.js

--- 1 - Own-KMeans.js
+++ 1 - Own-KMeans.js
@@ -23,7 +23,7 @@ class KMeans {
         this.element.ctx = this.element.getContext("2d");
         this.dataExtremes = this.getDataExtremes(this.data);
         
-        this.range = this.getDataRanges(this.dataExtremes); // get ranges from above 
+        this.range = this.getDataRanges(this.dataExtremes); // get ranges from extremes 
         // console.log(this.range) 9,10 - WORKS here!
         this.means = this.initMeans(3);
         this.assignPoints();
@@ -40,14 +40,15 @@ fill in ranges array from "data" array with the points, dimension is [x, y]
      const ranges = [];
         for (const dimension in extremes){
             ranges[dimension] = extremes[dimension].max - extremes[dimension].min;
+            // 10 - 1 => 9, 11 - 1 => 10
         }
 
-        return ranges; // we should see 9, 10
+    return ranges; // [9, 10]
     }
     
     
     
-    // pass in data, get ranges
+    // get border points
     getDataExtremes(points){
       const extremes = [];
         
@@ -58,7 +59,7 @@ fill in ranges array from "data" array with the points, dimension is [x, y]
         for (const dimension in point){
             
             if (!extremes[dimension]){
-                extremes[dimension] = {min: 1000, max:0}
+                extremes[dimension] = {min: 1000, max:0} // changing those values
             }
             
             if (point[dimension] < extremes[dimension].min){
@@ -75,7 +76,7 @@ fill in ranges array from "data" array with the points, dimension is [x, y]
         
 
         console.log(extremes)
-        return extremes;
+        return extremes; // {min: 1, max: 10} {min:1, max:1}
         
     }
     
@@ -83,12 +84,57 @@ fill in ranges array from "data" array with the points, dimension is [x, y]
     
     initMeans(k = 3){
         // init means
+        
+        while(k--){
+            const mean = [];
+            
+            for (const dimension in this.dataExtremes){
+                mean[dimension] = this.dataExtremes[dimension].min + (Math.random() * this.range[dimension]);
+            }
+            
+            this.means.push(mean);
+        }
+        console.log(this.means);
+        return this.means; // 3 array, each has 2 numbers in it (random)
     }
     
+    
+    // Really understand!!!
+    
+/*------------------------------------------------------ASSIGN POINTS------------------------------------------------------
+1) calculate distance between each point and the cluster center
+2) assign all data points to the centroid closest to it
+*/
+
     assignPoints(){
-        // assign points
+        
+    for (const i in data){
+        const point = data[i];
+        const distances = [];
+        
+        for (const j in this.means){
+            const mean = this.means[j];
+            let sum = 0;
+            
+            for (const dimension in point){
+                let diff = point[dimension] - mean[dimension];
+                diff *= diff;
+                sum += diff;
+            }
+            
+            distances[j] = Math.sqrt(sum);
+        }
+        
+        this.assignments[i] = distances.indexOf(Math.min.apply(null, distances));
+        console.log(this.assignments[i]); // 0s 1s 2s
+        
+    }
+
     }
     
+/* MOVE MEANS
+move centroids
+*/ 
     moveMeans(){
     // move means    
     }
@@ -136,5 +182,5 @@ const data = [
 let el = new KMeans(data, "canvas");
 el.make();
 // ranges wont work here
-    
 // <canvas width="400" height="400"></canvas>
+   
