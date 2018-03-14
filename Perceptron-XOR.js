69df699ca46430692545232a1c4f90d391ad0f64
Filip Vabrousek <filipvabrousek1@gmail.com>
2018-03-14 22:29:16 +0100

    Update Perceptron-XOR.js

--- Perceptron-XOR.js
+++ Perceptron-XOR.js
@@ -1,67 +1,64 @@
-class Perceptron {
+
+
+class Perceptrona {
 	constructor() {
 		this.weights = [];
-		this.treshold = 1;
-		this.learningrate = 0.1; // used in adjust
-		this.threshold = 1;
 		this.data = [];
 	}
 
 
-	// called first
+// called first
 	train(inputs, expected) { // [0, 1], 1
 		while (this.weights.length < inputs.length) {
 			this.weights.push(Math.random())
 		}
 
-
-
-		// get result and push it to data ("use percieve")
-		const result = this.perceive(inputs); // result from sigmoid (weights are passed in), used to calculate delta
-		this.data.push({
-			input: inputs,
-			target: expected,
-			prev: result
-		});
-		if (result == expected) {
-			return true
+		
+        // add the bias
+		if (this.weights.length == inputs.length) {
+			this.weights.push(1);
 		}
 
-		// adjust weights id result is not expected using "adjust"
-		this.weights.forEach((el, index) => {
-			const input = (index == inputs.length) ? this.treshold : inputs[index];
-			this.adjust(result, expected, input, index)
-		});
-		return false
-	}
+        // get result and push it to data ("use percieve")
+		const result = this.perceive(inputs); // result from sigmoid (weights are passed in), used to calculate delta
+		this.data.push({input: inputs, target: expected, prev: result});
+		
 
-	// uses delta, adds delta to weights
-	adjust(result, expected, input, index) {
-		const d = (expected - result) * input * this.learningrate;
+        // adjust weights id result is not expected using "adjust"
+        this.weights.forEach((el, index) => {
+            const input = (index == inputs.length) ? 1 : inputs[index];
+            this.adjust(result, expected, input, index)
+			});
+   
+	}
+    
+
+    // add elta to weights 0.1 is learning rate
+    adjust(result, expected, input, index) {
+		const d = (expected - result) * input * 0.1;
 		this.weights[index] += d;
 	}
-
-
-	/*
-	called 2nd, use "train" function, this.data:
-	{input: [0,0], target: 1, prev: 0.97} 
-	{input: [0,1], target: 0, prev: 0.049} periodically swaps
-	*/
+    
+/*
+called 2nd, use "train" function, this.data:
+{input: [0,0], target: 1, prev: 0.97}
+{input: [0,1], target: 0, prev: 0.049} periodically swaps
+*/
 	retrain() {
-		let success = true;
+		let scs = true;
 		this.data.forEach(a => {
 			const training = this.data.shift(); // 1st el during 1st repeatition, then 2nd, 3rd ---
-			success = this.train(training.input, training.target) && success;
+			scs = this.train(training.input, training.target) && scs;
 		});
-		return success
+		return scs
 	}
+    
 
-
-	// called 3rd, get sum of weighed inputs and pass them into sigmoid
+    // called 3rd, get sum of weighed inputs and pass them into sigmoid
 	perceive(inputs) {
 		let result = 0;
-		inputs.forEach((el, index) => result += inputs[index] * this.weights[index]);
-		result += this.threshold * this.weights[this.weights.length - 1];
+		inputs.forEach((el, index) => result += inputs[index] *  this.weights[index]);
+		result += 1 * this.weights[this.weights.length - 1];
 		return this.sigmoid(result); // determine if neuron fires
 	}
 
@@ -71,14 +68,15 @@ class Perceptron {
 }
 
 
-const p = new Perceptron();
-p.train([0, 0], 0);
-p.train([0, 1], 1);
+const pa = new Perceptrona();
+pa.train([0, 0], 0);
+pa.train([0, 1], 1);
 
-// practice makes perfect (we hope...)
+// practice....
+for (let i = 0; i < 10000; i++){
+    pa.retrain();
+}
 
-let i = 0;
-while (i++ < 10000 && !p.retrain()) {}
-
-let res = p.perceive([0, 1]);
-console.log(res);
+let small = pa.perceive([0, 0]);
+let big = pa.perceive([0, 1]); 
+console.log(small, big);
