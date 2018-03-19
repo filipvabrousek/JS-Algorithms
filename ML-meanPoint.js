ca5993a4ece6f839c8205c411546be526afce1cd
Filip Vabrousek <filipvabrousek1@gmail.com>
2018-03-19 08:23:54 +0100

    Update ML-meanPoint.js

--- ML-meanPoint.js
+++ ML-meanPoint.js
@@ -1,23 +1,10 @@
+
 class Point {
     constructor(x, y) {
         this.x = x;
         this.y = y;
     }
 }
-
-const points = [
-    new Point(10, 70),
-    new Point(20, 50),
-    new Point(200, 90),
-    new Point(100, 120),
-    new Point(20, 80),
-    new Point(90, 10),
-    new Point(200, 100),
-    new Point(80, 120),
-    new Point(70, 130)
-];
-
-
 
 class Means {
     constructor(selector) {
@@ -33,8 +20,26 @@ class Means {
         this.points = points;
     }
 
-
-
+// get data extremes
+    getExtremes(){
+        let xm = 0, ym = 0, xn = 0, yn = 0;
+        this.points.forEach((el, i) => {
+             xm = this.points.map(e => e.x).reduce((a, b) => Math.max(a, b));
+             ym = this.points.map(e => e.y).reduce((a, b) => Math.max(a, b));
+             xn = this.points.map(e => e.x).reduce((a, b) => Math.min(a, b));
+             yn = this.points.map(e => e.y).reduce((a, b) => Math.min(a, b));
+        });
+          return {xmax: xm, ymax: ym, xmin: xn, ymin: yn} // return as array
+    }
+    
+    getRanges(){
+        let ranges = this.getExtremes(), res = [];
+        let xr = ranges.xmax - ranges.xmin;
+        let yr = ranges.ymax - ranges.ymin;
+        res.push(xr);
+        res.push(yr);
+        return res;
+    }
 
     getMean() {
         const meanX = this.points.map(p => p.x).reduce((a, b) => a + b) / this.points.length;
@@ -46,8 +51,7 @@ class Means {
     }
 
     draw() {
-        let points = this.points,
-            ctx = this.ctx;
+        let points = this.points, ctx = this.ctx;
         this.ctx.fillStyle = "#000";
 
         points.forEach((el, i) => {
@@ -66,9 +70,23 @@ class Means {
     }
 }
 
+const points = [
+    new Point(10, 70),
+    new Point(20, 50),
+    new Point(200, 90),
+    new Point(100, 120),
+    new Point(20, 80),
+    new Point(90, 10),
+    new Point(200, 700),
+    new Point(80, 120),
+    new Point(70, 130)
+];
+
+
+
 let m = new Means("canvas");
 m.init(points);
 m.draw();
 m.getMean();
-
-// <canvas width = "400" height="400"></canvas>
+let r = m.getRanges();
+console.log(r);
