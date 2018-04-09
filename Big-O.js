c87853c1bf95e230a59a531d810b337360a5b15e
Filip Vabrousek <filipvabrousek1@gmail.com>
2018-04-09 17:16:15 +0200

    Create Big-O.js

---
+++ Big-O.js
@@ -0,0 +1,27 @@
+// O(1) - constant time complexity
+// accessing array index    
+    
+    
+// O(n) - linear time complexity
+// for loop over array
+    
+// O(log n) - logaritmic time complexity
+// i = i * 2
+    
+    
+for (var i = 1; i < n; i = i * 2)
+  console.log(i);
+}
+
+
+
+// O(n^2) - quadratic time complexity
+    
+for(var i = 0; i < length; i++) {     //has O(n) time complexity
+    for(var j = 0; j < length; j++) { //has O(n^2) time complexity
+      // More loops?
+    }
+}    
+    
+    
+// bubble sort  
