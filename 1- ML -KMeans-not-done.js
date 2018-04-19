2e8988b96236e7252043be9000587be21bcb5ed1
Filip Vabrousek <filipvabrousek1@gmail.com>
2018-04-19 19:01:59 +0200

    Update and rename KMeans-17-04-18.js to 1- ML -KMeans-not-done.js

--- 1- ML -KMeans-not-done.js
+++ 1- ML -KMeans-not-done.js
@@ -62,54 +62,79 @@ class Means {
     /* pushing points either into APoints / BPoints according to distance from each centroid use "getRanges"
     used in "drawMeans()"*/
     assign(centrs) {
-        let APoints = [],
-        BPoints = []
-       ,assignments = [],
-         distances = [],
-       sum = 0;
+        let APoints = [],BPoints = [],assignments = [],distances = [], sum = 0;
 
         points.forEach((val, i) => {
 
         let point = points[i];
 
         centrs.forEach((val, e) => {
+                
+            
                 let diffX = (point.x - centrs[e].x) / 10;
                 let diffY = (point.y - centrs[e].y) / 10;
-                sum += Math.sqrt(diffX * diffX) + Math.sqrt(diffY * diffY);
+                sum += Math.sqrt(diffX * diffX);
+                sum += Math.sqrt(diffY * diffY);
 
                 let obj = {
                         pointIndex: i,
                         dist: Math.round(sum),
                         to: e
                 }
-                distances.push(obj);
-
+                distances.push(obj); 
         });
 
 });
+        
+          
+           
 
 
         let oneClose = [];
         let secondClose = [];
 
         
-        
         distances.forEach((el, i) => {
-           // if (i > distances.length - 1){i = distances.length - 1} work with i
-            // what is the distance to to first and to second 
+                
+            if (distances[i + 1]){
+                
+            
             let toFirst = distances[i].dist;
-            let toSecond = distances[i + 1].dist; // should be d + 1
-console.log(toSecond)
-            if (toFirst < toSecond) {
-                console.log("Closer to the first centroid ", toFirst)
-                oneClose.push(points[i]);
+            let toSecond = distances[i + 1].dist; 
+            
+            if (toFirst < toSecond) { // WHY IS THIS ALWAYS TRUE ???????
+                console.log("Closer to the first centroid ", toFirst);
+                oneClose.push(points[distances[i].pointIndex]);
             } else {
                 console.log("Closer to the 2nd centroid ", toSecond);
-                secondClose.push(points[i]);
+                secondClose.push(points[distances[i].pointIndex]);
             }
-           // console.log(distances)
-        })
+           
+                console.log(oneClose)
+                } // security check
+        });
+        
+        
+         console.log("Centers0 ", centrs[0], "Centrs1 ", centrs[1]);
+        
+        this.ctx.fillStyle = "orange";
+        this.ctx.beginPath();
+        this.ctx.arc(centrs[0].x, centrs[0].x, 6, 0, 2 * Math.PI);
+        this.ctx.fill();
+        
+        this.ctx.fillStyle = "blue";
+        this.ctx.beginPath();
+        this.ctx.arc(centrs[1].x, centrs[1].x, 6, 0, 2 * Math.PI);
+        this.ctx.fill();
 
+        
+        
+        oneClose.forEach((el, i) => {
+            this.ctx.beginPath();
+            this.ctx.arc(oneClose[i].x, oneClose[i].y, 4, 0, 2 * Math.PI);
+            this.ctx.fill();
+        });
+        
         return distances;
       
     }
@@ -120,15 +145,6 @@ console.log(toSecond)
           
     }
 
-    // get mean of all assigned points (mean point of APoints and BPoints array), do: if len === 0 condition
-    assignedPointsMean(arr, len) {
-        const meanX = arr.map(p => p.x).reduce((a, b) => a + b) / len;
-        const meanY = arr.map(p => p.y).reduce((a, b) => a + b) / len;
-        return {
-            x: meanX,
-            y: meanY
-        }
-    }
 
 
     // plot the points we define in our array on the canvas and recolor them according to assigned points
@@ -148,9 +164,25 @@ console.log(toSecond)
     }
 
 
+/*
 
+    // get mean of all assigned points (mean point of APoints and BPoints array), do: if len === 0 condition
+    assignedPointsMean(arr, len) {
+        const meanX = arr.map(p => p.x).reduce((a, b) => a + b) / len;
+        const meanY = arr.map(p => p.y).reduce((a, b) => a + b) / len;
+        return {
+            x: meanX,
+            y: meanY
+        }
+    }
+
+
+
+
+
+*/
     /* calculate mean point with assigned points and draw each cluster centroid
-    if we have classified all the points */
+    if we have classified all the points 
     drawMean() {
         let ctx = this.ctx;
         let means = this.initMeans(); // init ranodm means
@@ -213,19 +245,27 @@ console.log(toSecond)
             ctx.fill();
         });
     }
-
+*/
 
 
     // try to cluster the points, if "done" in findMeans is false, try again
-    try () {
-        this.getRanges();
+   /* try () {
+     this.getRanges();
         this.plot();
 
         let finished = this.drawMean();
         if (!finished) {
             setTimeout(this.try.bind(this), 500);
         }
+    }*/
+    
+    try(){
+        this.plot();
+        let means = this.initMeans();
+        this.assign(means);
     }
+    
+    
 }
 
 const points = [
