6bd307d7a75f102c474df9b93c2eb6fc59f8b9df
Filip Vabrousek <filipvabrousek1@gmail.com>
2018-04-26 20:48:28 +0200

    Update 1 ML-Perceptron.js

--- 1 ML-Perceptron.js
+++ 1 ML-Perceptron.js
@@ -1,63 +1,57 @@
-class Perceptron{
-    constructor(){
+class Perceptron {
+    constructor() {
         this.weights = [];
-        this.data = [];   
+        this.arr = [];
     }
-    
-    fill(){
+
+    fill() {
         this.weights.length = 2; // 2
         this.weights.fill(Math.random());
-        
-        if (this.weights.length == 2){
+
+        if (this.weights.length == 2) {
             this.weights.push(1);
         }
     }
-    
-    train(inputs, expected){
-     
-        const res = this.calc(inputs);
-        this.data.push({input: inputs, target: expected});
-        
-        this.weights.forEach((el, i) =>{
-            const input = (i == inputs.length) ? 1 : inputs[i]; 
-            let diff = expected - res;
+
+    train(data) { // [[0, 1], 1]
+        const res = this.calc(data[0]); // inputs data 0 ... expected data 1
+        this.arr.push(data)
+        this.weights.forEach((el, i) => {
+            const input = (i == data[0].length) ? 1 : data[0][i];
+            let diff = data[1] - res;
             this.weights[i] += diff * input * 0.1;
         });
-        
+
     }
-    
-    calc(inputs){
-    let res = 0;
-    inputs.forEach((el, i) => res += this.weights[i] * inputs[i]);
-    res += this.weights[this.weights.length - 1]; // bias, different each time
-    return this.sigmoid(res);
+
+    calc(inputs) {
+        let res = 0;
+        inputs.forEach((el, i) => res += this.weights[i] * inputs[i]);
+        res += this.weights[this.weights.length - 1]; // bias, different each time
+        return this.sigmoid(res);
     }
-    
-    
-    retrain(){
-        this.data.forEach(a => {
-            const training = this.data.shift();
-            this.train(training.input, training.target);
-        });
+
+
+    retrain() {
+        this.arr.forEach(e => this.train(this.arr.shift()));
     }
-    
-    learn(){
-        for (let i = 0; i < 1000; i++){
+
+    learn() {
+        for (let i = 0; i < 1000; i++) {
             this.retrain();
         }
     }
-    
-    sigmoid(x){
+
+    sigmoid(x) {
         return 1 / (1 + Math.exp(-x));
     }
 }
 
-let p = new Perceptron();
+
+const p = new Perceptron();
 p.fill();
-p.train([0, 0], 0);
-p.train([0, 1], 1);
+p.train([[0, 1], 1]);
 p.learn();
-    
-let small = p.calc([0, 0]);
-let big = p.calc([0, 1]);
-console.log(small,big);
+
+let res = p.calc([0, 1]);
+console.log(res);
