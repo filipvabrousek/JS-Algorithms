3f8721776d76b43777824b4566d86b34e48a914c
Filip Vabrousek <filipvabrousek1@gmail.com>
2018-06-24 17:44:36 +0200

    Update 2 - disemvowel-kata.swift

--- Katas/2 - disemvowel-kata.swift
+++ Katas/2 - disemvowel-kata.swift
@@ -5,7 +5,6 @@ func disemvowel(_ str: String) -> String { // test for 3.1 (str.characters)
     var res = ""
     for i in 0..<str.count {
        
-        
         let index = str.index(str.startIndex, offsetBy: i)
         let char = str[index]
         
@@ -24,3 +23,13 @@ disemvowel("This website is for losers LOL!")
 
 // "This website is for losers LOL!"
 // => "Ths wbst s fr lsrs LL!".
+
+
+/*
+ OR:
+ func disemvowel(_ s: String) -> String {
+ let vowels: [Character] = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+ return String(s.characters.filter { !vowels.contains($0) })
+ }
+ */
+
