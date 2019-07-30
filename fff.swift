5658b0ca0769bf4385c4238c2610a2270802b9ee
Filip Vabrousek <filipvabrousek1@gmail.com>
2019-07-30 15:07:38 +0200

    Update fff.swift

--- fff.swift
+++ fff.swift
@@ -1,23 +1,77 @@
 //
 //  AppDelegate.swift
-//  CorrectDelegate
+//  RelayWorld
 //
-//  Created by Filip Vabroušek on 30/07/2019.
+//  Created by Filip Vabroušek on 16/07/2019.
 //  Copyright © 2019 Filip Vabroušek. All rights reserved.
 //
 
 import UIKit
+import SwiftUI
 import CoreData
+import Firebase
+import FirebaseMessaging
+import FirebaseInstanceID
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
+
+        window?.tintColor = hex("#e67e22")
+
+        UNUserNotificationCenter.current().delegate = self
+        let options: UNAuthorizationOptions = [.alert, .badge, .sound]
+
+        UNUserNotificationCenter.current().requestAuthorization(options: options) { (ok, err) in
+            if err != nil {
+                print("PUSH Error \(String(describing: err?.localizedDescription))")
+            } else {
+                print("OK \(ok)")
+            }
+        }
+
+        application.registerForRemoteNotifications()
+        Messaging.messaging().delegate = self
+        UIApplication.shared.registerForRemoteNotifications()
         return true
     }
+
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+
+        let data = response.notification.request.content.userInfo
+        let to = data["gcm.notification.to"]!
+        print("BOND25 \(to)")
+        window?.rootViewController = UIHostingController(rootView: SelectView())
+    }
+    
+
+
+    // do not show completion handler in chat view
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+
+        /* if let ud = UserDefaults.standard.value(forKey: "isinchat") as? Bool {
+            UserDefaults.standard.set("OP", forKey: "highlight")
+            print("174 RECE!")
+
+            if ud == true {
+                completionHandler([])
+            } else {
+                completionHandler([.alert, .sound])
+            }
+        } */
+
+
+        completionHandler([.alert, .sound])
+    }
+
+
+
 
     // MARK: UISceneSession Lifecycle
 
@@ -42,12 +96,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
         */
-        let container = NSPersistentContainer(name: "CorrectDelegate")
+        let container = NSPersistentContainer(name: "RelayWorld")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
+
                 /*
                  Typical reasons for an error here include:
                  * The parent directory does not exist, cannot be created, or disallows writing.
@@ -78,5 +132,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
     }
 
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String) {
+        let token: [String: AnyObject] = [Messaging.messaging().fcmToken!: Messaging.messaging().fcmToken! as AnyObject]
+        let db = Database.database().reference()
+        db.child("fcmAuth").child(Messaging.messaging().fcmToken!).setValue(token)
+    }
+
+
+
+
 }
+
+
+extension AppDelegate {
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.portrait
+    }
+}
+
+
 
